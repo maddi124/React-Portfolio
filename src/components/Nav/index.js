1e552fe3d1860a88a452d9dab4c309e09915d705
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-20 22:25:31 -0400

    Files

---
+++ src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Nav () {
+return (
+<header>
+    <nav>
+        <ul>
+            <il>
+                <a href="#about-me">About Me</a>
+            </il>
+            <il>
+                <a href="#work">Work</a>
+            </il>
+            <il>
+                <a href="#contact-me">Contact Me</a>
+            </il>
+        </ul>
+    </nav>
+</header>
+
+
+);
+
+}
+
+export default Nav;
