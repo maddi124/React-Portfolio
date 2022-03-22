04660f7a105a0c72b536e0a1c01de14ee807d6df
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-21 21:43:28 -0400

    add a Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Nav from './components/Nav';
-import Home from './components/Homepage'
+import Home from './components/Homepage';
+import Footer from './components/Footer';
 import './assets/style.css';
+
 function App() {
 return (
   <div>
   <Nav></Nav>
     <main>
 <Home></Home>
+<Footer/>
     </main>
   </div>
 );
