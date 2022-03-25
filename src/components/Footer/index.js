379d616e40c7f015f2cf8c64294c498bf4532982
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-24 20:02:35 -0400

    icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import {GoMarkGithub } from "react-icons/go";
-
-
+import {BsLinkedin} from "react-icons/bs";
+import {AiFillTwitterCircle} from "react-icons/ai";
 function Footer(){
 return (
-    <div className='container'>
-    <div className='row'>
-        <div className='col'>
-            <GoMarkGithub/>
+<div className='containers '>
+    <div className='row justify-content-center'>
+        <div className='col-1'>
+            <GoMarkGithub size="50px"/>
+        </div> 
+        <div className='col-1'>
+            <BsLinkedin size="50px"/>
+        </div>
+        <div className='col-1'>
+            <AiFillTwitterCircle size="50px"/>
         </div>
     </div>
-
-    
-  </div>
+</div>
 
 );
 }
