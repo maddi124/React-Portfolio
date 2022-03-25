bd5a12b894cee0bd98a2e59204849feb8b9ea1f8
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-24 22:37:17 -0400

    icon links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,19 +8,25 @@ return (
 <div className='containers '>
     <div className='row justify-content-center'>
         <div className='col-1'>
+        <a href="https://github.com/maddi124">
             <GoMarkGithub 
                 size="50px" 
                 color="#9EC5AB"/>
+        </a>
         </div> 
         <div className='col-1'>
+        <a href="https://www.linkedin.com/in/madeline-garcia-cabrera-790580210/">
             <BsLinkedin 
                 size="50px"  
                 color="#9EC5AB"/>
+        </a>
         </div>
         <div className='col-1'>
+        <a href="https://twitter.com">
             <AiFillTwitterCircle 
                 size="50px" 
                 color="#9EC5AB"/>
+        </a>        
         </div>
     </div>
 </div>
