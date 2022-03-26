fc45df15896be9e3ecc0fe31ce90e2ad10eb6e56
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-26 15:37:38 -0400

    projects added

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -5,7 +5,7 @@ import avatarImage from "../../assets/cover/avatar.png";
 function AboutMe() {
 return (
 <section>
-    <div class="container">
+    <div class="container homepage">
     <div class="box">
         <h5 class="aboutme">My name is Madeline. I have a associate's degree in Computer Engineering. I'm currently changing my career to web development. I enrolled in this coding bootcamp to learn skills that would help me create an <strong>AMAZING</strong> websites for companies or individual. I am a team-player and excellent problem solver. I may be a beginner, but I'm always ready to learn.</h5>
     </div>
--- src/components/Workpage/index.js
+++ src/components/Workpage/index.js
@@ -1,8 +1,41 @@
 import React from "react";
+import ProjectImage1 from "../../assets/cover/projectholder4.png";
+import ProjectImage2 from "../../assets/cover/Case_tracker.png";
+import ProjectImage3 from "../../assets/cover/note-taker.png";
 
 function Workpage() {
 return (
-    <p>Work Page</p>
+<div class="container projectbox">
+    <h1 class="worktitle">Some Of My Work</h1>
+    <div class="row ">
+        <div class="col-lg-4 col-md-12 mb-4">
+            <img class="card-img-top" src={ProjectImage1}  alt="Running-Buddy"/>
+            <div class="">
+                <h5>Running Buddy</h5>
+                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="col">
+            <img class="card-img-top " src={ProjectImage2}  alt="Case-Tracker"/>
+            <div class="">
+                <h5>Case Tracker</h5>
+                <p>This application is a kanban for a company to use to track employee cases.
+The user story is: An employer would like to keep track of it's employees exposure to Covid19. It uses this information to make sure that the employee is quarentened for the correct number of days to keep others safe and to stop the spread of Covid19. 
+It also uses this information to pass onto the contact tracing office who job it is to notify potential employees of exposure. Each employee case is managed initially through the employee's direct magager and this information is included in each case. The date of sympotms and positive testing are also documented. 
+The kanban is organized with cases under the catagories of pending and completed. </p>
+            </div>
+        </div>
+        <div class="col">
+        <img class="card-img-top" src={ProjectImage3}  alt="Running-Buddy"/>
+            <div class="">
+                <h5>Note Taker</h5>
+                <p> We were given a starter code for an application called Note Taker that used to write ans save notes. We had to use an Express.js back-end that will save and retrieve note data from a JSON file.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
 );
 }
 export default Workpage;
