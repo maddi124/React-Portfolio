37f90d6fbadc006c449441f2c711f5c46c4ebad4
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-25 20:39:21 -0400

    contact form

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -2,7 +2,22 @@ import React from "react";
 
 function ContactMe() {
 return (
-    <p>Contact me</p>
+<div class="container">
+<h3>Contact Form</h3>  
+    <form>
+        <div class="form-group">
+            <label for="fname" placeholder="First Name">First Name</label>
+            <input type="text" class="form-control" id="fname" placeholder="First Name"/>
+        </div>
+        <div class="form-group">
+            <label for="fname" placeholder="Last Name">Last Name</label>
+            <input type="text" class="form-control" id="fname" placeholder="Last Name"/>
+        </div>
+        <div class="form-group">
+        <textarea class="form-control" id="exampleFormControlTextarea1" ></textarea>
+        </div>
+    </form>
+</div>
 );
 }
 export default ContactMe;
