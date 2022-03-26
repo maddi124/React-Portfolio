dd1f10a7f81dc0f103bda15d545117e9edb46859
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-26 18:27:25 -0400

    fade

--- src/components/Workpage/index.js
+++ src/components/Workpage/index.js
@@ -9,32 +9,38 @@ return (
 
     <h1 class="workheader">Some Of My Work</h1>
 
-    <div class="row ">
-        <div class="col">
-            <img class="card-img-top" src={ProjectImage1} data-toggle="modal" data-target="#mymodal" alt="Running-Buddy"/>
-            <div class="workinfo">
-                <h5>Running Buddy</h5>
-                <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="row ">
+            <div class="col fade">
+                <a href="https://lernantino.github.io/run-buddy/"> 
+                    <img class="card-img-top" src={ProjectImage1}  alt="Running-Buddy"/>
+                </a>
+                <div class="workinfo">
+                    <h5>Running Buddy</h5>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="col fade">
+                <a href="https://fathomless-cliffs-14331.herokuapp.com/"> 
+                    <img class="card-img-top " src={ProjectImage2}  alt="Case-Tracker"/>
+                </a>
+                <div class="workinfo">
+                    <h5>Case Tracker</h5>
+                    <p>This application is a kanban for a company to use to track employee cases.
+    The user story is: An employer would like to keep track of it's employees exposure to Covid19. It uses this information to make sure that the employee is quarentened for the correct number of days to keep others safe and to stop the spread of Covid19. 
+    It also uses this information to pass onto the contact tracing office who job it is to notify potential employees of exposure. Each employee case is managed initially through the employee's direct magager and this information is included in each case. The date of sympotms and positive testing are also documented. 
+    The kanban is organized with cases under the catagories of pending and completed. </p>
+                </div>
+            </div>
+            <div class="col fade">
+                <a href="https://young-anchorage-76744.herokuapp.com/">
+                    <img class="card-img-top" src={ProjectImage3}  alt="Running-Buddy"/>
+                </a>
+                <div class="workinfo">
+                    <h5>Note Taker</h5>
+                    <p> We were given a starter code for an application called Note Taker that used to write ans save notes. We had to use an Express.js back-end that will save and retrieve note data from a JSON file.</p>
+                </div>
             </div>
         </div>
-        <div class="col">
-            <img class="card-img-top " src={ProjectImage2}  alt="Case-Tracker"/>
-            <div class="workinfo">
-                <h5>Case Tracker</h5>
-                <p>This application is a kanban for a company to use to track employee cases.
-The user story is: An employer would like to keep track of it's employees exposure to Covid19. It uses this information to make sure that the employee is quarentened for the correct number of days to keep others safe and to stop the spread of Covid19. 
-It also uses this information to pass onto the contact tracing office who job it is to notify potential employees of exposure. Each employee case is managed initially through the employee's direct magager and this information is included in each case. The date of sympotms and positive testing are also documented. 
-The kanban is organized with cases under the catagories of pending and completed. </p>
-            </div>
-        </div>
-        <div class="col">
-        <img class="card-img-top" src={ProjectImage3}  alt="Running-Buddy"/>
-            <div class="workinfo">
-                <h5>Note Taker</h5>
-                <p> We were given a starter code for an application called Note Taker that used to write ans save notes. We had to use an Express.js back-end that will save and retrieve note data from a JSON file.</p>
-            </div>
-        </div>
-    </div>
 
 </div>
 
