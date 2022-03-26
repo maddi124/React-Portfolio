b2d646fadfdcfef4fc96d120fd1821b5e35a6980
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-25 22:34:09 -0400

    submit button

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -2,20 +2,26 @@ import React from "react";
 
 function ContactMe() {
 return (
-<div class="container">
-<h3>Contact Form</h3>  
+<div class="container contactform ">
+<h3 class="textform">Contact Form</h3>  
     <form>
         <div class="form-group">
             <label for="fname" placeholder="First Name">First Name</label>
             <input type="text" class="form-control" id="fname" placeholder="First Name"/>
         </div>
         <div class="form-group">
-            <label for="fname" placeholder="Last Name">Last Name</label>
-            <input type="text" class="form-control" id="fname" placeholder="Last Name"/>
+            <label for="Lname" placeholder="Last Name">Last Name</label>
+            <input type="text" class="form-control" id="Lname" placeholder="Last Name"/>
         </div>
         <div class="form-group">
-        <textarea class="form-control" id="exampleFormControlTextarea1" ></textarea>
+            <label for="phone" placeholder="Phone Number">Phone Number</label>
+            <input type="text" class="form-control" id="phone" placeholder="(###)###-####"/>
         </div>
+        <div class="form-group">
+        <label for="message" placeholder="message">Message</label>
+        <textarea class="form-control" id="message" ></textarea>
+        </div>
+        <input type="submit" class="submit"value="Submit"/>
     </form>
 </div>
 );
