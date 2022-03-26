3126cdf09fdb16e4100636459742d4e4699bff82
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-26 16:35:05 -0400

    color

--- src/components/Workpage/index.js
+++ src/components/Workpage/index.js
@@ -6,18 +6,20 @@ import ProjectImage3 from "../../assets/cover/note-taker.png";
 function Workpage() {
 return (
 <div class="container projectbox">
-    <h1 class="worktitle">Some Of My Work</h1>
+
+    <h1 class="workheader">Some Of My Work</h1>
+
     <div class="row ">
-        <div class="col-lg-4 col-md-12 mb-4">
-            <img class="card-img-top" src={ProjectImage1}  alt="Running-Buddy"/>
-            <div class="">
+        <div class="col">
+            <img class="card-img-top" src={ProjectImage1} data-toggle="modal" data-target="#mymodal" alt="Running-Buddy"/>
+            <div class="workinfo">
                 <h5>Running Buddy</h5>
                 <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
         <div class="col">
             <img class="card-img-top " src={ProjectImage2}  alt="Case-Tracker"/>
-            <div class="">
+            <div class="workinfo">
                 <h5>Case Tracker</h5>
                 <p>This application is a kanban for a company to use to track employee cases.
 The user story is: An employer would like to keep track of it's employees exposure to Covid19. It uses this information to make sure that the employee is quarentened for the correct number of days to keep others safe and to stop the spread of Covid19. 
@@ -27,7 +29,7 @@ The kanban is organized with cases under the catagories of pending and completed
         </div>
         <div class="col">
         <img class="card-img-top" src={ProjectImage3}  alt="Running-Buddy"/>
-            <div class="">
+            <div class="workinfo">
                 <h5>Note Taker</h5>
                 <p> We were given a starter code for an application called Note Taker that used to write ans save notes. We had to use an Express.js back-end that will save and retrieve note data from a JSON file.</p>
             </div>
