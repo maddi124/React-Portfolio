8a58186fb1b8ff5c7d41c71a51d8034d5d18c4d5
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-27 13:59:16 -0400

    download

--- src/components/ResumePage/index.js
+++ src/components/ResumePage/index.js
@@ -1,21 +1,23 @@
 
 import React from "react";
+import {BsDownload} from "react-icons/bs";
+import resumedownload from "../../assets/cover/resume/RESUME_2021.pdf";
 
 function ResumePage() {
 return (
 <div class="container">
     <div class="row">
         <h2>Education</h2>
-        <div class="col">
-            <p>Associate in Applied Science, Computer Engineering Technology, York Technical College, Rock Hill, 
+        <div class="col edinfo">
+            <p class="resp">Associate in Applied Science, Computer Engineering Technology, York Technical College, Rock Hill, 
 SC. Graduated May 2021, GPA- 2.82</p>
-<p>The Coding Boot Camp at UNC Charlotte, Graduating April 2022 </p>
+<p class="resp">The Coding Boot Camp at UNC Charlotte, Graduating April 2022 </p>
         </div>
     </div>
       <h2 class="skills">Skills</h2>
     <div class="row">
       <h3>HTML/CSS</h3>
-        <div class="col">
+        <div class="col-8 long">
             <div class="progress">
                 <div class="progress-bar w-75"  role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
             </div>
@@ -23,7 +25,7 @@ SC. Graduated May 2021, GPA- 2.82</p>
     </div>
     <div class="row">
       <h3>JavaScript/jQuery</h3>
-        <div class="col">
+        <div class="col-8 ">
             <div class="progress">
                 <div class="progress-bar w-50"  role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
             </div>
@@ -31,7 +33,7 @@ SC. Graduated May 2021, GPA- 2.82</p>
     </div>
     <div class="row">
       <h3>MERN Stack</h3>
-        <div class="col">
+        <div class="col-8 long">
             <div class="progress">
                 <div class="progress-bar w-75"  role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
             </div>
@@ -39,12 +41,15 @@ SC. Graduated May 2021, GPA- 2.82</p>
     </div>
     <div class="row">
       <h3>MySQL</h3>
-        <div class="col">
+        <div class="col-8 long">
             <div class="progress">
                 <div class="progress-bar w-50"  role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
             </div>
         </div>
     </div>
+    <a href={resumedownload} download>
+    <button class="btn"><BsDownload/> Download Resume</button>
+    </a>
 </div>
 );
 }
