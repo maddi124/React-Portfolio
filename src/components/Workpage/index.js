ca4540c2f885010868ecfc30b314fdd5ec17f3a7
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-03-26 23:44:32 -0400

    fade

--- src/components/Workpage/index.js
+++ src/components/Workpage/index.js
@@ -10,20 +10,20 @@ return (
     <h1 class="workheader">Some Of My Work</h1>
 
         <div class="row ">
-            <div class="col fade">
+            <div class="col ">
                 <a href="https://lernantino.github.io/run-buddy/"> 
-                    <img class="card-img-top" src={ProjectImage1}  alt="Running-Buddy"/>
+                    <img class="card-img-top " src={ProjectImage1}  alt="Running-Buddy"/>
                 </a>
-                <div class="workinfo">
+                <div class="workinfo fade">
                     <h5>Running Buddy</h5>
                     <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
-            <div class="col fade">
+            <div class="col ">
                 <a href="https://fathomless-cliffs-14331.herokuapp.com/"> 
                     <img class="card-img-top " src={ProjectImage2}  alt="Case-Tracker"/>
                 </a>
-                <div class="workinfo">
+                <div class="workinfo fade">
                     <h5>Case Tracker</h5>
                     <p>This application is a kanban for a company to use to track employee cases.
     The user story is: An employer would like to keep track of it's employees exposure to Covid19. It uses this information to make sure that the employee is quarentened for the correct number of days to keep others safe and to stop the spread of Covid19. 
@@ -31,11 +31,11 @@ return (
     The kanban is organized with cases under the catagories of pending and completed. </p>
                 </div>
             </div>
-            <div class="col fade">
+            <div class="col">
                 <a href="https://young-anchorage-76744.herokuapp.com/">
-                    <img class="card-img-top" src={ProjectImage3}  alt="Running-Buddy"/>
+                    <img class="card-img-top " src={ProjectImage3}  alt="Running-Buddy"/>
                 </a>
-                <div class="workinfo">
+                <div class="workinfo fade">
                     <h5>Note Taker</h5>
                     <p> We were given a starter code for an application called Note Taker that used to write ans save notes. We had to use an Express.js back-end that will save and retrieve note data from a JSON file.</p>
                 </div>
